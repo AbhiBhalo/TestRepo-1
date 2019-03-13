f75e1465ceb2397f9b174af971511bc53493d1ef
AdityaSingh17 <asaditya140@gmail.com>
2019-03-13 13:53:07 +0530

    Added addition statement.

---
+++ Demo/src/Calculate.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author devillord17
+ *
+ */
+public class Calculate {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		System.out.println("addition");
+		System.out.println(a+b);
+		// TODO Auto-generated method stub
+
+	}
+
+}
